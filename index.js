c4bef4a88dd62fc722ed25fb3d5e5f16e1a6efd9
jeont <jeont@seas.upenn.edu>
2020-05-11 14:22:59 -0400

    Adds heroku scheduler

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ const connectDB = require('./services/db');
 const express = require('express');
 const bodyParse = require('body-parser');
 const mongoose = require('mongoose');
-const cron = require('node-cron');
-const nodemailer = require('nodemailer');
 const path = require('path');
-
-const Friendship = require('./models/Friendship');
-const User = require('./models/User');
 
 const app = express();
 
@@ -19,94 +14,6 @@ connectDB();
 // Use bodyparser
 app.use(bodyParse.json());
 app.use(bodyParse.urlencoded({ extended: true }));
-
-cron.schedule('* * * * *', async () => {
-  const reminderTime = new Date(Date.now() - 60 * 1000);
-
-  const friendships = await Friendship.find({
-    lastSeen: { $lte: reminderTime },
-  }).populate({ path: 'users', model: 'User', select: 'name email' });
-
-  console.log(friendships);
-
-  const reminders = {};
-
-  friendships.map((friendship) => {
-    if (!friendship.reminded) {
-      const [user1, user2] = friendship.users;
-      const user1Id = user1._id;
-      const user2Id = user2._id;
-      if (!reminders[user1Id]) {
-        reminders[user1Id] = {};
-        reminders[user1Id].people = [user2];
-        reminders[user1Id].email = user1.email;
-        reminders[user1Id].name = user1.name;
-      } else {
-        reminders[user1Id].people.unshift(user2);
-      }
-      if (!reminders[user2Id]) {
-        reminders[user2Id] = {};
-        reminders[user2Id].people = [user1];
-        reminders[user2Id].email = user2.email;
-        reminders[user2Id].name = user2.name;
-      } else {
-        reminders[user2Id].people.unshift(user1);
-      }
-    }
-  });
-
-  // Configure nodemailer
-
-  // Configure nodemailer
-
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'intouchmailerapp@gmail.com',
-      pass: process.env.EMAILPASSWORD,
-    },
-  });
-
-  for (const key in reminders) {
-    if (reminders.hasOwnProperty(key)) {
-      user = reminders[key];
-      console.log('user: ' + JSON.stringify(user));
-
-      const mailOptions = {
-        from: 'intouchmailerapp@gmail.com',
-        to: user.email,
-        subject: 'Your inTouch Reminders',
-        text: `Make sure to hang out with: ${user.people.map(
-          (person) => person.name
-        )}`,
-      };
-
-      transporter.sendMail(mailOptions, function (err, res) {
-        if (err) {
-          console.error('there was an error: ', err);
-        } else {
-          console.log('here is the res: ', res);
-        }
-      });
-    }
-
-    const friendPromises = friendships.map((friendship) => {
-      friendship.reminded = true;
-      return new Promise((resolve, reject) => {
-        friendship.save((error, result) => {
-          if (error) {
-            reject(error);
-          }
-          resolve(result);
-        });
-      });
-    });
-
-    await Promise.all(friendPromises);
-  }
-
-  console.log(reminders);
-});
 
 // Define routes
 app.use('/api/auth', require('./routes/api/auth'));
